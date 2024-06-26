9f718363c8c055a95489412c42149a1a59d5ea3f
spacemandev <devbharel@gmail.com>
2024-06-26 12:56:39 -0500

    updated discord bot to use spsblink domain

--- server/discord.ts
+++ server/discord.ts
@@ -11,6 +11,7 @@ const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const rest = new REST().setToken(DISCORD_BOT_TOKEN);
 
+const url = "https://spsblink.runepunk.gg" // TODO change this to deployment URL
 const connection = new anchor.web3.Connection(process.env.RPC, "confirmed");
 const serverKey = anchor.web3.Keypair.fromSecretKey(Buffer.from(JSON.parse(readFileSync("./keys/A2UG3TvnBLjVb2uzz19igwfBN42soLXYHgQZe1TKFsV8.json").toString())))
 const program: anchor.Program<RplSpsBlinks> = new anchor.Program(idl, new anchor.AnchorProvider(connection, new anchor.Wallet(serverKey)));
@@ -62,7 +63,7 @@ const infoCommand = {
 
         try {
             const sps = await program.account.sps.fetch(spsKey);
-            const blink = `http://localhost:3000/corporation?q=${spsKey.toString()}`
+            const blink = `${url}/api/corporation?q=${spsKey.toString()}`
 
             await interaction.reply({
                 content: `
@@ -116,7 +117,7 @@ const incorporateCommand = {
                 }
             })
 
-            const blink = `http://localhost:3000/corporation?q=${spsKey.toString()}`
+            const blink = `${url}/api/corporation?q=${spsKey.toString()}`
             await interaction.reply({
                 content: `
                 Success! Here is your corporation blink: ${blink}
