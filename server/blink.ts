15d77ea86edf83f98c854976c5d1d868ea32f204
Kyle Pena <kylepena@Kyles-MacBook-Air.local>
2024-06-26 12:56:37 -0400

    removing blink_txn

--- server/blink.ts
+++ server/blink.ts
@@ -1,12 +1,22 @@
+import * as anchor from '@coral-xyz/anchor';
+import 'dotenv/config';
 import { cors } from 'hono/cors';
 import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
-import { ActionError, ActionGetResponse, ActionPostResponse } from './interfaces';
+import { ActionGetResponse, ActionPostResponse } from './interfaces';
 import { serveStatic } from '@hono/node-server/serve-static'
-import { makeCorporationBuyTxn } from './blink_txn';
+import { RplSpsBlinks } from './idl/rpl_sps_blinks'; 
+import { readFileSync } from 'fs';
+import { Corporation } from '@prisma/client';
+import { PublicKey } from '@solana/web3.js';
 
+const prisma = new PrismaClient();
+const idl = require("./idl/rpl_sps_blinks");
+console.log("RPC:", process.env.RPC);
+const connection = new anchor.web3.Connection(process.env.RPC, "confirmed");
+const program: anchor.Program<RplSpsBlinks> = new anchor.Program(idl, { connection });
+const serverKey = anchor.web3.Keypair.fromSecretKey(Buffer.from(JSON.parse(readFileSync("./keys/A2UG3TvnBLjVb2uzz19igwfBN42soLXYHgQZe1TKFsV8.json").toString())))
 const app = new Hono();
 
 app.use('/public/*', serveStatic({ root: "./" }));
@@ -96,8 +106,31 @@ serve({
 })
 
 
-
 console.log(`Hono running on port ${process.env.PORT || 3000}`);
+
+
+export async function makeCorporationBuyTxn(corp : Corporation, size : 1|2|3) : Promise<anchor.web3.VersionedTransaction> {
+    const priorityFeeIx = anchor.web3.ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1000 });
+    console.log(size);
+    const ix = await program.methods.buyGoods(convertToAnchorFormatEnum(size)).accounts({
+        sps: new PublicKey(corp.publickey)
+    }).instruction();
+    const msg = new anchor.web3.TransactionMessage({
+        payerKey: serverKey.publicKey,
+        recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
+        instructions: [priorityFeeIx, ix]
+    }).compileToV0Message();
+    const txn = new anchor.web3.VersionedTransaction(msg);
+    return txn;
+}
+
+function convertToAnchorFormatEnum(size : 1|2|3) : { small : {} }|{ medium : {}}|{ large : {}} {
+    return {
+        1: { small : {} },
+        2: { medium : {} },
+        3: { large: {} }
+    }[size];
+}
 
 function parseSizeOrThrow(size : string) : 1|2|3 {
     const parsed = parseInt(size,10);
